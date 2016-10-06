1eb7a56bb2aeae6008d37c0a7e132c8b539d61bd
Meghaben Patel <200321317@student.georgianc.on.ca>
2016-10-06 18:41:54 +0000

    lab3 script 5

--- bash/lab2/showinterfaces.sh
+++ bash/lab2/showinterfaces.sh
@@ -6,11 +6,13 @@
 #also using ifconfig output. Also parse the output of 
 #route -n to display the ip address of the default GATEWAY.
 
-interfacesnames=(`ifconfig | grep `^[a-zA-Z]` | amk '{print $1}'`)
+interfacesnames=(`ifconfig |grep '^[a-zA-Z]' |awk '{print $1}'`)
 
-
-ips=(`ifconfig ${interfacesnames[0]} | grep 'inet addr' | sed -e 's/ *inet addr://'|sed -e 's/ .*//'`)
+declare -a ips
+ips[0]=`ifconfig ${interfacesnames[0]} | grep 'inet addr' | sed -e 's/ *inet addr://'|sed -e 's/ .*//'`
 ips[1]=`ifconfig ${interfacesnames[1]} | grep 'inet addr' | sed -e 's/ *inet addr://'|sed -e 's/ .*//'`
+ips=($ips[0] $ips[1])
+gatewayip=`route -n |grep '^0.0.0.0'|awk '{print $2}'`
 
 cat <<EOF
 
